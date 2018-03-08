3ac3131ae2969c743cb4a7e3e76d6a0a509820f7
lazylife7157 <lazylife7157@gmail.com>
2018-03-08 12:27:44 +0900

    Fix syntax error

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut input = Scanner::from(Source::Stdin);
     let t = input.next_number();
     let result = input.next_n_lines(t)
-        .solve()
+        .map(solve)
         .enumerate()
         .map(format_single_line)
         .collect<Vec<String>>()
